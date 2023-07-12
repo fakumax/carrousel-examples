d8628d9b2b423421bbff77f635ec5c3054d8acdc
Facundo Vergara <fakumax@gmail.com>
2023-07-12 00:25:50 -0300

    Fix Responsive

--- src/components/CarrouselSimple.tsx
+++ src/components/CarrouselSimple.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './CarrouselSimple.css';
-const images = [
+
+const images: string[] = [
   'https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600',
   'https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
   'https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -9,7 +10,7 @@ const images = [
   'https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
 
-const CarrouselSimple = ({ images }) => {
+const CarrouselSimple = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
@@ -18,28 +19,30 @@ const CarrouselSimple = ({ images }) => {
   const handlePrevious = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1));
   };
-  const handleDotClick = (index) => {
+  const handleDotClick = (index: number) => {
     setCurrentIndex(index);
   };
   return (
     <div className="carousel">
-      <div className="carousel-images">
-        <img key={currentIndex} src={images[currentIndex]} />
-      </div>
-      <div className="slide_direction">
-        <div className="left" onClick={handlePrevious}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
-            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-          </svg>
+      <div className="image-container">
+        <div className="carousel-images">
+          <img key={currentIndex} src={images[currentIndex]} />
         </div>
-        <div className="right" onClick={handleNext}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
-            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-          </svg>
+        <div className="slide_direction">
+          <div className="left" onClick={handlePrevious}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+            </svg>
+          </div>
+          <div className="right" onClick={handleNext}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+            </svg>
+          </div>
         </div>
       </div>
       <div className="carousel-indicator">
-        {images.map((_, index) => (
+        {images.map((_, index: number) => (
           <div
             key={index}
             className={`dot ${currentIndex === index ? 'active' : ''}`}
