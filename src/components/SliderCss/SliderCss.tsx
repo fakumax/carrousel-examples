c00a0aeb3db60894eb25bb3970d97be1d99374e7
Facundo Vergara <fakumax@gmail.com>
2023-10-01 20:47:00 -0300

    Finis Css scroll snap

--- src/components/SliderCss/SliderCss.tsx
+++ src/components/SliderCss/SliderCss.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styles from './SliderCss.module.css';
 
 const images: string[] = [
@@ -11,45 +10,11 @@ const images: string[] = [
 ];
 
 const SliderCss = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1 === images.length ? 0 : prevIndex + 1));
-  };
-  const handlePrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1));
-  };
-  const handleDotClick = (index: number) => {
-    setCurrentIndex(index);
-  };
   return (
     <div className={styles.carousel}>
-      <div className={styles.imageContainer}>
-        <div className={styles.carouselImages}>
-          <img key={currentIndex} src={images[currentIndex]} />
-        </div>
-        <div className={styles.slide_direction}>
-          <div className={styles.left} onClick={handlePrevious}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
-              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-            </svg>
-          </div>
-          <div className={styles.right} onClick={handleNext}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
-              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-            </svg>
-          </div>
-        </div>
-      </div>
-      <div className={styles.carouselIndicator}>
-        {images.map((_, index: number) => (
-          <div
-            key={index}
-            className={`dot ${currentIndex === index ? 'active' : ''}`}
-            onClick={() => handleDotClick(index)}
-          ></div>
-        ))}
-      </div>
+      {images.map((image, index) => (
+        <img key={index} src={image} />
+      ))}
     </div>
   );
 };
